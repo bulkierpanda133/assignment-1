af75adbdf4a046e35d6be8bce57bb4cb258e36b5
bulkierpanda133 <elijahvick50@gmail.com>
2024-09-09 16:19:03 -0700

    first commit

---
+++ question 2 python name convention practice.py
@@ -0,0 +1,15 @@
+#this is not right
+
+
+Pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+
+#tack 1 this is right [code correction]
+
+PI_VALUE = 3.14     
+user_age = 25            
+user_location = "New York" 
+MAX_LIMIT = 1000     
---
+++ question 3 arithmetic operation in daily life.py
@@ -0,0 +1,40 @@
+#task 1 grocary math
+bread = (4.00) 
+jelly = (2.78)
+peanut_butter = (3.86)
+
+
+
+#calculate the total cost
+
+total_cost = bread + peanut_butter + jelly
+
+
+
+# Print the total cost
+print(f"the total cost is ${total_cost:.2f}")
+
+
+
+
+
+
+
+
+
+#task 2 bank interest
+saving_account = 10000 
+interest_rate = 0.07
+
+
+
+#calculate the total cost after 1 one year 
+total_amount = saving_account *(1 + interest_rate)
+
+print(f"the total amount after one year is $ {total_amount}")
+
+
+
+
+
+
